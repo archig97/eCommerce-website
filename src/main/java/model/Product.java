574bc6dd25091783d4da3e545bd414adacb1734e
archig97 <archismitaghosh@yahoo.com>
2025-02-27 20:31:38 -0500

    Product model updated

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class Product {
+
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    private String brand;
+    private Category category;
+    private List<Image> images;
 }
