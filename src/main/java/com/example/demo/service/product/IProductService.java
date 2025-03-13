94a045fae51edf23d7cf13e498de4543e60ca931
archig97 <archismitaghosh@yahoo.com>
2025-03-13 09:36:39 -0400

    product repository created

--- src/main/java/com/example/demo/service/product/IProductService.java
+++ src/main/java/com/example/demo/service/product/IProductService.java
@@ -1,4 +1,22 @@
 package com.example.demo.service.product;
 
+import com.example.demo.model.Product;
+
+import java.util.List;
+
 public interface IProductService {
+
+    Product addProduct(Product product);//for adding product to DB
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getProductByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String productName);
+    List<Product> getProductsByBrandAndName(String brand, String productName);
+    Long countProductsByBrandAndName(String brand, String productName);
+
+
 }
--- src/main/java/com/example/demo/service/product/ProductService.java
+++ src/main/java/com/example/demo/service/product/ProductService.java
@@ -1,4 +1,69 @@
 package com.example.demo.service.product;
 
-public class ProductService {
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService {
+
+    private ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getProductByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String productName) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String productName) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String productName) {
+        return 0L;
+    }
 }
