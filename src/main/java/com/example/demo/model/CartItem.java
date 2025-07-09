21da5d35d84bcaaf43e0b4d3c3604cfb1ae24d20
archig97 <archismitaghosh@yahoo.com>
2025-07-08 20:48:52 -0400

    Created entitiy classes for cart and cart items

---
+++ src/main/java/com/example/demo/model/CartItem.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice(){
+        this.totalPrice = this.unitPrice.multiply(new BigDecimal(this.quantity));
+    }
+}
